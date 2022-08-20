06e49a36ed626bcca6709d78717c53b9474b37fd
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-08-20 13:47:10 +0300

    dependences added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const myPlayer = new player(iframe);
+
+const onPlay = function () {
+  console.log(timeupdate);
+};
+
+myPlayer.on(
+  'play',
+  _.throttle(1000, () => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(timeupdate)
+    );
+    console.log('video started');
+  })
+);
+
+myPlayer.off('click', () => {
+  console.log(localStorage.getItem(timeupdate));
+});
+
+// myPlayer.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
