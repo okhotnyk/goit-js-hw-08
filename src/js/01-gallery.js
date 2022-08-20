cfa65ef0f76a64266b91065949b26e47b0117d97
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-08-20 13:47:45 +0300

    new lightbox added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import simplelightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const markUpGalleryList = createItemMarkUp(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', markUpGalleryList);
+
+function createItemMarkUp(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join('');
+}
+
+const myLightbox = new simplelightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
